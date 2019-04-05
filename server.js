476f10d617c1d9aa30738e52a6021f68268e85ca
vietanh <anhnv74.39@gmail.com>
2019-04-05 07:11:11 +0700

    dasdasdadad

--- server.js
+++ server.js
@@ -1,28 +1,67 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-Object.assign=require('object-assign')
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+//server.listen(3000);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 80
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
-
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
-var initDb = function(callback) {
-
-};
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
 
+    app.get("/", function(req, res)
+    	{
+    		res.render("HomePage");
+    	});
 
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+    	var mqtt = require('mqtt');
+    	var options = {
+    	    port: 12064,
+    	    host: 'mqtt://m16.cloudmqtt.com',
+    	    clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+    	    username: 'dsbwiuis',
+    	    password: 'glc9YQlmbcJS',
+    	    keepalive: 60,
+    	    reconnectPeriod: 1000,
+    	    protocolId: 'MQIsdp',
+    	    protocolVersion: 3,
+    	    clean: true,
+    	    encoding: 'utf8'
+    	};
 
-module.exports = app ;
+    	var client = mqtt.connect('mqtt://m16.cloudmqtt.com', options);
+
+    	client.on('connect', () => {
+    	  client.subscribe('topic1')
+    	})
+
+    	function ab2str(buf) {
+    	  return String.fromCharCode.apply(null, new Uint16Array(buf));
+    	}
+
+      // tạo kết nối giữa client và server
+    io.on("connection", function(socket)
+    {
+    			//Nhân dữ liệu từ cliet
+    			socket.on("TURN_ON",function(data){
+    				//Gửi dữ liệu đến MQTT
+    					client.publish('topic2', 'ON')
+    			});
+    			//Nhân dữ liệu từ cliet
+    			socket.on("TURN_OFF",function(data){
+    					//Gửi dữ liệu đến MQTT
+    					client.publish('topic2', 'OFF')
+    			});
+          //Nhận dữ liệu từ server MQTT
+    			client.on('message', (topic, message)=>{
+    				//console.log("Received '" + message + "' on '" + topic + "'");
+    				//Gửi dữ liệu cho client
+    				socket.emit("Server-sent-data", ab2str(message));
+    			})
+
+    });
