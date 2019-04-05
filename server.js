8b2b0084db7207a5a34fb5b7d61c16873fc25d0d
vietanh <anhnv74.39@gmail.com>
2019-04-05 07:46:23 +0700

    Dasdas

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set("views", "./views");
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 //server.listen(3000);
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 80
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 server.listen(server_port, server_ip_address, function () {
